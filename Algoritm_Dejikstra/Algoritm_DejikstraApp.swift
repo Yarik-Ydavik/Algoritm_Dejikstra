4a22812456eef757f208ee77156b2194668a6b52
Yaroslav Zagumennikov <91648112+Yarik-Ydavik@users.noreply.github.com>
2023-07-16 00:08:36 +0500

    Initial Commit

---
+++ Algoritm_Dejikstra/Algoritm_DejikstraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Algoritm_DejikstraApp.swift
+//  Algoritm_Dejikstra
+//
+//  Created by Yaroslav Zagumennikov on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Algoritm_DejikstraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
